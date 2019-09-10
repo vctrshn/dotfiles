994dc09ab2bc9144a8ddea9245fbb4ffcc48749d
Victor Shen <vctrshn@gmail.com>
2019-09-10 09:22:07 -0700

    Remove ubersicht

--- ubersicht/widgets/all-the-things.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-timeData=$(date "+%R")
-dayData=$(date "+%A")
-dateData=$(date "+%m/%d/%y")
-networkData=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep ' SSID' | cut -c 18-38)
-batteryData=$(pmset -g batt | grep "%" | awk 'BEGINN { FS = ";" };{ print $3,$4 }' | sed -e 's/-I/I/' -e 's/-0//' -e 's/;//' -e 's/;//')
-
-echo "$timeData@$dayData@$dateData@$networkData@$batteryData"
--- ubersicht/widgets/workspaces.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-currentMonitor=$(/Users/vshen/homebrew/bin/chunkc tiling::query --monitor id)
-currentWorkspace=$(/Users/vshen/homebrew/bin/chunkc tiling::query --desktop id)
-allWorkspaces=$(/Users/vshen/homebrew/bin/chunkc tiling::query --desktops-for-monitor $currentMonitor)
-
-echo "$currentWorkspace@$allWorkspaces"
-

62b8d3f06da7591dbd41c1be78c1b6f73972cfdd
Victor Shen <vshen@fb.com>
2025-07-04 17:06:12 -0700

    init.vim -> init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,37 @@
+vim.cmd([[
+  set runtimepath^=~/.vim runtimepath+=~/.vim/after
+  let &packpath = &runtimepath
+  source ~/.vimrc
+
+  let g:python3_host_prog='/usr/local/bin/python3'
+
+  " Live preview of substitution command
+  set inccommand=split
+
+  " Unlimited scrollback in terminal
+  set scrollback=-1
+
+  tnoremap <C-\> <C-\><C-n>
+  tnoremap <S-Left> <C-\><C-n>gT
+  tnoremap <S-Right> <C-\><C-n>gt
+
+  augroup terminal
+    autocmd!
+    autocmd TermOpen * setlocal nonumber winfixheight winfixwidth
+    autocmd BufEnter term://* startinsert
+  augroup end
+
+  " Neoterm config
+  let g:neoterm_autoinsert = 1
+  let g:neoterm_default_mod = 'tab'
+  nnoremap <C-s> :Ttoggle<CR>
+  tnoremap <C-s> <C-\><C-n>:Ttoggle<CR>
+  cnoreabbrev TT Topen \| T
+]])
+
+if not vim.g.vscode then
+  require('treesitter-config')
+  require('lsp-config')
+  require('compe-config')
+  pcall(require, 'work')
+end
